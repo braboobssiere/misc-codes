ff3e2d853706da02f97f8f5c76ac792dd09b2896
Boobs <86918103+braboobssiere@users.noreply.github.com>
2024-02-24 03:35:28 +0000

    Add files via upload

---
+++ user-overrides.js
@@ -0,0 +1,29 @@
+
+ user_pref("browser.cache.memory.enable", false);
+ user_pref("browser.cache.memory.capacity", 0);
+
+ user_pref("security.cert_pinning.enforcement_level", 0); // 2 > 0
+ user_pref("privacy.clearOnShutdown.siteSettings", true); 
+ user_pref("privacy.clearOnShutdown.cookies", false); 
+ user_pref("webgl.disabled", false); 
+
+ user_pref("signon.rememberSignons", false); 
+ user_pref("browser.urlbar.suggest.history", false);
+ user_pref("browser.urlbar.suggest.openpage", false);
+ user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
+ user_pref("browser.taskbar.lists.enabled", false);
+ user_pref("browser.taskbar.lists.frequent.enabled", false);
+ user_pref("browser.taskbar.lists.recent.enabled", false);
+ user_pref("browser.taskbar.lists.tasks.enabled", false);
+ user_pref("alerts.useSystemBackend.windows.notificationserver.enabled", false);
+
+ user_pref("permissions.default.geo", 2);
+ user_pref("permissions.default.camera", 2);
+ user_pref("permissions.default.microphone", 2);
+ user_pref("permissions.default.desktop-notification", 2);
+ user_pref("permissions.default.xr", 2);
+ user_pref("browser.urlbar.autoFill", false);
+ user_pref("places.history.enabled", false);
+ user_pref("extensions.formautofill.addresses.enabled", false); // [FF55+]
+ user_pref("extensions.formautofill.creditCards.enabled", false); // [FF56+]
+

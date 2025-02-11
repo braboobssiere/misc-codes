5db21f71289c0fcedd0f736fa02666e758dfc30c
Boobs <86918103+braboobssiere@users.noreply.github.com>
2025-02-11 12:40:27 +0700

    Update index.js

--- .github/actions/vpn/index.js
+++ .github/actions/vpn/index.js
@@ -1,35 +1,10 @@
-const { exec } = require('child_process');
-
-if (process.env.INPUT_VPN_ENABLED === 'true') {
-  // Connect to VPN
-  exec('sudo apt-get update && sudo apt-get install -y openvpn openvpn-systemd-resolved', (err, stdout, stderr) => {
-    if (err) {
-      console.error('Error installing OpenVPN:', err);
-      return;
-    }
-    console.log(stdout);
-    exec('echo "${{ secrets.OPENVPN_CONFIG }}" > vpnconfig.ovpn && sudo openvpn vpnconfig.ovpn', (err, stdout, stderr) => {
-      if (err) {
-        console.error('Error connecting to VPN:', err);
-        return;
-      }
-      console.log(stdout);
-    });
-  });
-} else {
-  // Check if OpenVPN is running, then disconnect
-  exec('pgrep openvpn', (err, stdout, stderr) => {
-    if (err) {
-      console.log('OpenVPN is not running, skipping disconnection.');
-      return;  // If OpenVPN is not running, we don't need to disconnect
-    }
-    // If OpenVPN is running, disconnect it
-    exec('sudo pkill openvpn', (err, stdout, stderr) => {
-      if (err) {
-        console.error('Error disconnecting VPN:', err);
-        return;
-      }
-      console.log(stdout);
-    });
-  });
-}
+name: VPN Setup
+description: 'Connects to VPN and disconnects based on input'
+inputs:
+  vpn_enabled:
+    description: 'Enable VPN connection? (true/false)'
+    required: true
+    type: string
+runs:
+  using: "node12"
+  main: "index.js"

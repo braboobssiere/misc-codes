25fcf0781521943b5a5e37adf51f6bbb1ccd2970
Boobs <86918103+braboobssiere@users.noreply.github.com>
2026-04-28 21:10:11 +0700

    Fix sw path (#161)

--- js/fairshare-script.js
+++ js/fairshare-script.js
@@ -910,7 +910,7 @@ function init() {
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/fairshare-sw.js')
+        navigator.serviceWorker.register('../fairshare-sw.js')
             .then(reg => console.log('Service Worker registered:', reg))
             .catch(err => console.error('Service Worker registration failed:', err));
     });
